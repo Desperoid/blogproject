904d8ae2ddec2d96b2e187b992f24444bdad6ecd
Desperoid <623016526@qq.com>
2018-12-11 23:46:26 +0800

    简单分页

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name = 'index'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name= 'detail'),
-    url(r'^archives/(?P<year>\d{4})/(?P<month>\d{1,2})/$',views.archives, name='archives'),
-    url(r'^category/(?P<pk>\d+)/$',views.category, name='category'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name= 'detail'),
+    url(r'^archives/(?P<year>\d{4})/(?P<month>\d{1,2})/$',views.ArchivesView.as_view(), name='archives'),
+    url(r'^category/(?P<pk>\d+)/$',views.CategoryView.as_view(), name='category'),
 ]
